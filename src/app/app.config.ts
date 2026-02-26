b4930c7395434075ff0d1aaf6c5bf5578b58ea91
Wilson Biggs <wab41@pitt.edu>
2026-02-26 12:00:05 -0500

    Remove unused import

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, APP_INITIALIZER, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
